47c2d2aed80e3c86b26ada6acd9e87b146fab343
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2021-03-16 15:36:05 +0100

    Merge

--- include/wish.h
+++ include/wish.h
@@ -6,4 +6,37 @@
 
 #include "scanner.h"
 
-#define INPUT_BUFFER_SIZE 256
+/**
+ * Initializes list for holding execution arguments for commands.
+ * Returned pointer points to malloced area
+ * and must be freed after use.
+ * @param size Number of arguments that can be held by list.
+ * @returns Pointer to argument list
+ */
+char **initArgList(size_t size);
+
+/**
+ * Resizes argument list
+ * @param arg_list Argument list to be resized
+ * @param new_size New list size
+ * @returns Pointer to resized list
+ */
+char **resizeArgList(char **arg_list, size_t new_size);
+
+/**
+ * Inserts string into arg_list at index.
+ * Arguments should always be inserted at the end of the argument list.
+ * Inserting anywhere else will cause a memory leak.
+ * @param arg String to be inserted. Modifying passed string after execution will not affect the argument.
+ * @param arg_list Argument list to insert string into
+ * @param index Insertion index
+ */
+void argListInsert(char *arg, char **arg_list, long index);
+
+/**
+ * Free argument list and all associated arguments
+ * @param arg_list Argument list to be freed
+ */
+void freeArgList(char **arg_list);
+
+#define BASE_ARGLIST_SIZE 8
--- src/wish.c
+++ src/wish.c
@@ -44,39 +44,33 @@ int main(int argc, char **argv)
             continue;
         }
 
+        size_t arg_list_size = 8;
+        size_t arg_count = 0;
+        char **arg_list = initArgList(arg_list_size);
+
         YY_BUFFER_STATE flex_buffer = yy_scan_string(input);
         int token = yylex();
-        char *command = NULL;
-
-        int option_count = 1;
-        char *options[256] = {NULL};
+        token = yylex();
         while (token != END)
         {
             switch (token)
             {
-                case STRING:
+            case STRING:
+            {
+                if (arg_count >= arg_list_size)
                 {
-                    // First token is always the command
-                    if (command == NULL)
-                    {
-                        command = (char *)malloc(sizeof(char *) * yyleng);
-                        strcpy(command, yytext);
-                        // Command options cannot be NULL, and by convention, argv[0] is the name of the command/program being run
-                        options[0] = command;
-
-                        break;
-                    }
-
-                    // The rest are options
-                    char *option = (char *)malloc(sizeof(char *) * yyleng);
-                    strcpy(option, yytext);
-                    options[option_count++] = option;
-
-                    break;
+                    arg_list_size = arg_list_size * 2;
+                    arg_list = resizeArgList(arg_list, arg_list_size);
                 }
+                argListInsert(yytext, arg_list, arg_count);
+                arg_count++;
+                break;
+            }
             }
             token = yylex();
         }
+        
+        free(input);
         yy_delete_buffer(flex_buffer);
 
         // Execute the command with the given options in a child process
@@ -88,10 +82,10 @@ int main(int argc, char **argv)
 
         if (child_pid == FORK_IN_CHILD)
         {
-            int status = execvp(command, options);
+            int status = execvp(arg_list[0], arg_list);
             if (status == -1)
             {
-                perror(command);
+                perror(arg_list[0]);
             }
             exit(status);
         }
@@ -107,13 +101,62 @@ int main(int argc, char **argv)
             #endif
         }
 
-        // Don't leak memory :)
-        // Note: the command is freed in the first iteration
-        for (int i = 0; i < option_count; i++)
-        {
-            free(options[i]);
-        }
-        free(input);
+        freeArgList(arg_list);
     }
     return 0;
+}
+
+/**
+ * Initializes list for holding execution arguments for commands.
+ * Returned pointer points to malloced area
+ * and must be freed after use.
+ * @param size Number of arguments that can be held by list.
+ * @returns Pointer to argument list
+ */
+char **initArgList(size_t size)
+{
+    char **arg_list = (char **)calloc(size + 1, sizeof(char *));
+    arg_list[0] = NULL;
+    return arg_list;
+}
+
+/**
+ * Resizes argument list
+ * @param arg_list Argument list to be resized
+ * @param new_size New list size
+ * @returns Pointer to resized list
+ */
+char **resizeArgList(char **arg_list, size_t new_size)
+{
+    arg_list = realloc(arg_list, sizeof(char*) * (new_size + 1));
+    return arg_list;
+}
+
+/**
+ * Inserts string into arg_list at index.
+ * Arguments should always be inserted at the end of the argument list.
+ * Inserting anywhere else will cause a memory leak.
+ * @param arg String to be inserted. Modifying passed string after execution will not affect the argument.
+ * @param arg_list Argument list to insert string into
+ * @param index Insertion index
+ */
+void argListInsert(char *arg, char **arg_list, long index)
+{
+    arg_list[index] = strdup(arg);
+    arg_list[index + 1] = NULL;
+}
+
+/**
+ * Free argument list and all associated arguments
+ * @param arg_list Argument list to be freed
+ */
+void freeArgList(char **arg_list)
+{
+    int i = 0;
+    while (arg_list[i] != NULL)
+    {
+        free(arg_list[i]);
+        i++;
+    }
+    free(arg_list);
 }
